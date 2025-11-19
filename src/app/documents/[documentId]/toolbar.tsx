c687f39702c86d8aa0dc1ed2884658d66f049dc5
ashwin biju <ashwinbiju011@gmail.com>
2025-11-19 21:19:08 +0530

    text-size

--- src/app/documents/[documentId]/toolbar.tsx
+++ src/app/documents/[documentId]/toolbar.tsx
@@ -10,6 +10,8 @@ import { BoldIcon,
     MessageSquarePlusIcon,  
     ListTodoIcon, 
     RemoveFormattingIcon } from "lucide-react";
+import { Level } from "@tiptap/extension-heading";
+
 import { cn } from "@/lib/utils";
 import { useEditorStore } from '@/store/use-editor-store';
 import { Separator } from "@/components/ui/separator";
@@ -21,6 +23,65 @@ import {
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 
+const HeadingLevelButton = () => {
+    const { editor } = useEditorStore();
+    const headings = [
+        { label: "Normal Text", value: 0, fontSize: "16px" },
+        { label: "Heading 1", value: 1, fontSize: "32px" },
+        { label: "Heading 2", value: 2, fontSize: "24px" },
+        { label: "Heading 3", value: 3, fontSize: "18px" },
+        { label: "Heading 4", value: 4, fontSize: "16px" },
+        { label: "Heading 5", value: 5, fontSize: "13px" },
+        { label: "Heading 6", value: 6, fontSize: "11px" },
+    ];
+
+    const getCurrentHeading = () => {
+        for (let level=1; level <= 5; level++) {
+            if (editor?.isActive("heading", { level })) {
+                return `Heading ${level}`;
+            }
+        }
+        return "Normal Text";
+    };
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <button
+                className=
+                "h-7 min-w-7 shrink-0 flex items-center justify-center rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm"
+                >
+                    <span className="truncate">
+                        {getCurrentHeading()}
+                        </span>
+                        <ChevronDownIcon className="ml-2 size-4 shrink-0 "/>
+                </button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="p-1 flex flex-col gap-y-1">
+                {headings.map(({ label, value, fontSize }) => (
+                    <button
+                    key={value}
+                    style={{ fontSize }}
+                    onClick={ () => {
+                        if (value === 0) {
+                            editor?.chain().focus().setParagraph().run();
+                        } else {
+                            editor?.chain().focus().toggleHeading({ level: value as Level}).run();
+                        }
+                    }}
+                     className={cn(
+                        "flex items-center gap-x-2 px-2 py-1 rounded-sm hover:bg-neutral-200/80",
+                       (value === 0 && !editor?.isActive("heading")) || editor?.isActive("heading", { level: value }) && "bg-neutral-200/80" 
+                    )}
+                    >
+                        {label}
+                    </button>
+                ))}
+
+            </DropdownMenuContent>
+            </DropdownMenu>
+    );
+
+};
 
 const FontFamilyButton = () => {
     const { editor } = useEditorStore();
@@ -33,6 +94,48 @@ const FontFamilyButton = () => {
         { label: "Times New Roman", value: "Times New Roman"},
         { label: "Verdana", value: "Verdana"},
         { label: "Courier New", value: "Courier New"},
+
+        { label: "Calibri", value: "Calibri" },
+        { label: "Cambria", value: "Cambria" },
+        { label: "Candara", value: "Candara" },
+        { label: "Century Gothic", value: "Century Gothic" },
+        { label: "Consolas", value: "Consolas" },
+        { label: "Constantia", value: "Constantia" },
+        { label: "Corbel", value: "Corbel" },
+        { label: "Franklin Gothic Medium", value: "Franklin Gothic Medium" },
+        { label: "Garamond", value: "Garamond" },
+        { label: "Gill Sans", value: "Gill Sans" },
+        { label: "Helvetica", value: "Helvetica" },
+        { label: "Lucida Console", value: "Lucida Console" },
+        { label: "Lucida Sans", value: "Lucida Sans" },
+        { label: "Optima", value: "Optima" },
+        { label: "Palatino", value: "Palatino" },
+        { label: "Segoe UI", value: "Segoe UI" },
+        { label: "Trebuchet MS", value: "Trebuchet MS" },
+        { label: "Rockwell", value: "Rockwell" },
+        { label: "Book Antiqua", value: "Book Antiqua" },
+        { label: "Monaco", value: "Monaco" },
+
+        { label: "Bookman Old Style", value: "Bookman Old Style" },
+        { label: "Berlin Sans FB", value: "Berlin Sans FB" },
+        { label: "Bahnschrift", value: "Bahnschrift" },
+        { label: "Century Schoolbook", value: "Century Schoolbook" },
+        { label: "Ebrima", value: "Ebrima" },
+        { label: "Gadugi", value: "Gadugi" },
+        { label: "High Tower Text", value: "High Tower Text" },
+        { label: "Javanese Text", value: "Javanese Text" },
+        { label: "Leelawadee UI", value: "Leelawadee UI" },
+        { label: "Malgun Gothic", value: "Malgun Gothic" },
+        { label: "Microsoft Himalaya", value: "Microsoft Himalaya" },
+        { label: "Microsoft JhengHei", value: "Microsoft JhengHei" },
+        { label: "Microsoft YaHei", value: "Microsoft YaHei" },
+        { label: "Myanmar Text", value: "Myanmar Text" },
+        { label: "Nirmala UI", value: "Nirmala UI" },
+        { label: "Sans Serif Collection", value: "Sans Serif Collection" },
+        { label: "Segoe Print", value: "Segoe Print" },
+        { label: "Segoe Script", value: "Segoe Script" },
+        { label: "Symbol", value: "Symbol" },
+        { label: "Yu Gothic UI", value: "Yu Gothic UI" }
     ];
     return (
         <DropdownMenu>
@@ -178,7 +281,7 @@ export const Toolbar = () => {
                <Separator orientation="vertical" className="h-6 bg-neutral-300" />
                <FontFamilyButton />
                <Separator orientation="vertical" className="h-6 bg-neutral-300" />
-               {/* TODO: Heading */}
+               {<HeadingLevelButton />}
                <Separator orientation="vertical" className="h-6 bg-neutral-300" />
                {/* TODO: Font size */}
                <Separator orientation="vertical" className="h-6 bg-neutral-300" />
