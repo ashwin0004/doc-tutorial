1b8cc566d07949ed90216d34656e4a5e5f997034
ashwin biju <ashwinbiju011@gmail.com>
2025-11-20 21:51:59 +0530

    add-no-on-top-of-the-page

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -21,10 +21,14 @@ import { useEditorStore } from '@/store/use-editor-store';
 import StarterKit from '@tiptap/starter-kit'
 
 import { FontSizeExtension } from '@/extensions/font-size';
+import { LineHeightExtension } from '@/extensions/line-height';
+import { Ruler } from './ruler';
 
 export const Editor = () => {
   const { setEditor } = useEditorStore();
+
      const editor = useEditor({
+      immediatelyRender: false,
       onCreate( {editor}) {
         setEditor(editor);       
       },
@@ -58,6 +62,7 @@ export const Editor = () => {
     },
     extensions: [
       StarterKit,
+      LineHeightExtension,
       TextStyle,
       FontSizeExtension,
       TextAlign.configure({
@@ -103,10 +108,11 @@ export const Editor = () => {
         </table>
       `,
     // Don't render immediately on the server to avoid SSR issues
-    immediatelyRender: false,
+    
   })
     return ( 
         <div className="size-full overflow-x-auto bg-[#F9FBFD] px-4 print:p-0 print:bg-white print:overflow-visible">
+            <Ruler />
             <div className="min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0">
             <EditorContent editor={editor} />
             </div>
--- src/app/documents/[documentId]/toolbar.tsx
+++ src/app/documents/[documentId]/toolbar.tsx
@@ -28,7 +28,8 @@ import {
     UploadIcon,
     SearchIcon,
     ListIcon,
-    PlusIcon} from "lucide-react";
+    PlusIcon,
+    ListCollapseIcon} from "lucide-react";
 
 import { cn } from "@/lib/utils";
 import { useEditorStore } from '@/store/use-editor-store';
@@ -50,6 +51,50 @@ import {
 
 import { Input} from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+
+
+const LineHeightButton = () => {
+    const { editor } = useEditorStore();
+
+    const lineHeights = [
+    { label: "Default", value: "normal" },
+    { label: "Single", value: "1" },
+    { label: "1.15", value: "1.15" },
+    { label: "1.3", value: "1.3" },
+    { label: "1.5", value: "1.5" },
+    { label: "1.75", value: "1.75" },
+    { label: "Double (2)", value: "2" },
+    ];
+    return (
+       < DropdownMenu>
+
+        <DropdownMenuTrigger asChild>
+            <button
+            className="h-7 min-w-7 shrink-0 flex flex-col items-center justify-center rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm"
+            >  
+            <ListCollapseIcon className="size-4"/>
+            </button>
+
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="p-1 flex flex-col gap-y-1">
+            {lineHeights.map(({ label, value}) => (
+                <button
+                key={value}
+                onClick={() => editor?.chain().focus().setLineHeight(value).run()}
+                className={cn(
+                    "flex items-center gap-x-2 px-2 py-1 rounded-sm hover:bg-neutral-200/80",
+                    editor?.getAttributes("paragraph").lineHeight === value && "bg-neutral-200/80"
+                )}
+                >
+                    <span className="text-sm">{label}</span>
+                </button>
+            ))}
+
+        </DropdownMenuContent>
+      </DropdownMenu>
+    );
+
+}
 
 
 const FontSizeButton = () => {
@@ -123,7 +168,8 @@ const FontSizeButton = () => {
                 setIsEditing(true);
                 setFontSize(currentFontSize);
             }}
-            className="h-7 w-10 text-sm text-center border border-neutral-400 rounded-sm bg-transparent cursor-text"
+            className="h-7 w-10 text-sm text-center border border-neutral-400 rounded-sm bg-transparent cursor-text" 
+                //   hover:bg-neutral-200/80 if you want one click or hover to edit
             >
                 {currentFontSize}
 
@@ -720,7 +766,7 @@ export const Toolbar = () => {
                 <LinkButton />
                 <ImageButton />
                 <AlignButton />
-                {/* TODO: Line Height*/}
+                <LineHeightButton />
                 <ListButton />
                 {sections[2].map((item) => (
                 <ToolbarButton
