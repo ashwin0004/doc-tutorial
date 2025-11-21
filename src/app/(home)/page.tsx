01345a6427af846040ef62e7898f93b5e60955f9
ashwin biju <ashwinbiju011@gmail.com>
2025-11-21 14:47:43 +0530

    navbar-tools

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+
 const Home = () => {
   return (
     <div className="flex min-h-screen items-center justify-center ">
--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -90,26 +90,9 @@ export const Editor = () => {
         nested: true,
       }),
       TaskList
-    ],
-    content: `
-        <table>
-          <tbody>
-            <tr>
-              <th>Name</th>
-              <th colspan="3">Description</th>
-            </tr>
-            <tr>
-              <td>Cyndi Lauper</td>
-              <td>Singer</td>
-              <td>Songwriter</td>
-              <td>Actress</td>
-            </tr>
-          </tbody>
-        </table>
-      `,
-    // Don't render immediately on the server to avoid SSR issues
-    
+    ],  
   })
+
     return ( 
         <div className="size-full overflow-x-auto bg-[#F9FBFD] px-4 print:p-0 print:bg-white print:overflow-visible">
             <Ruler />
