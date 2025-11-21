a2fd8b1525206a2c630798d14050bec4b01603f5
ashwin biju <ashwinbiju011@gmail.com>
2025-11-21 21:01:41 +0530

    added-templates-in-the-homepage

---
+++ src/constants/templates.ts
@@ -0,0 +1,57 @@
+export const templates = [
+    { 
+        id: "blank", 
+        label: "Blank Document", 
+        imageUrl: "/blank-document.svg",
+    },
+    { 
+        id: "software-proposal", 
+        label: "Software development proposal", 
+        imageUrl: "/software-proposal.svg",
+    },
+    { 
+        id: "project-proposal", 
+        label: "Project proposal", 
+        imageUrl: "/project-proposal.svg",
+    },
+    { 
+        id: "business-letter", 
+        label: "Business letter", 
+        imageUrl: "/business-letter.svg",
+    },
+    { 
+        id: "resume", 
+        label: "Resume", 
+        imageUrl: "/resume.svg",
+    },
+    { 
+        id: "cover-letter", 
+        label: "Cover letter", 
+        imageUrl: "/cover-letter.svg",
+    },
+    { 
+        id: "letter", 
+        label: "Letter", 
+        imageUrl: "/letter.svg",
+    },
+    { 
+        id: "report", 
+        label: "Report", 
+        imageUrl: "/Report.png",
+    },
+    { 
+        id: "recipe", 
+        label: "Recipe", 
+        imageUrl: "/Recipe.png",
+    },
+    { 
+        id: "brochure", 
+        label: "Brochure", 
+        imageUrl: "/Brochure.png",
+    },
+    { 
+        id: "class-note", 
+        label: "Class-note", 
+        imageUrl: "/Class-note.png",
+    },
+];
