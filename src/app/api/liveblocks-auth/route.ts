a853b1775d9aac35a6514eb132b62cd92b3194fa
ashwin biju <ashwinbiju011@gmail.com>
2025-11-26 14:11:27 +0530

    complete-collaboration-names-&errors

---
+++ src/app/api/liveblocks-auth/route.ts
@@ -0,0 +1,64 @@
+import { Liveblocks } from "@liveblocks/node";
+import { ConvexHttpClient } from "convex/browser";
+import { auth, currentUser } from "@clerk/nextjs/server";
+
+import { api } from "../../../../convex/_generated/api";
+
+const convex = new ConvexHttpClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+const liveblocks = new Liveblocks({
+    secret: process.env.LIVEBLOCKS_SECRET_KEY!,
+});
+// new (chat gpt)
+type ClerkOrgClaims = {
+  o?: {
+    id?: string;
+    rol?: string;
+    slg?: string;
+  };
+};
+
+export async function POST(req: Request) {
+    const { sessionClaims } = await auth();
+    if (!sessionClaims) {
+        return new Response("Unauthorized", { status: 401});
+    }
+// new
+    const claims = sessionClaims as ClerkOrgClaims;
+
+
+    const user = await currentUser();
+
+    // last remove cheyan ee thazhai ollth
+    console.log({ sessionClaims })
+    if (!user) {
+        return new Response("Unauthorized", { status: 401});
+    }
+
+    const { room } = await req.json();
+    const document = await convex.query(api.documents.getById, { id: room});
+
+    if (!document) {
+        return new Response("Unauthorized", { status: 401 });
+    }
+
+    const isOwner = document.ownerId === user.id;
+    const isOrganizationMember =
+  !!(document.organizationId && document.organizationId === claims.o?.id);
+
+
+
+    if (!isOwner && !isOrganizationMember) {
+        return new Response("Unauthorized", { status: 401 });
+    }
+
+    const session = liveblocks.prepareSession(user.id, {
+        userInfo: {
+            name: user.fullName ?? "Anonymous",
+            avatar: user.imageUrl,
+        },
+    });
+    session.allow(room, session.FULL_ACCESS);
+    const { body, status } = await session.authorize();
+
+    return new Response(body, { status });
+};
--- src/app/documents/[documentId]/room.tsx
+++ src/app/documents/[documentId]/room.tsx
@@ -12,7 +12,10 @@ export function Room({ children }: { children: ReactNode }) {
     const params = useParams();
 
   return (
-    <LiveblocksProvider publicApiKey={"pk_dev_cToA4LruWBfw-14zh-xrsfaZVqvUgQxuMEFggudvI6J9_aTJ1DNOu3L95OB2vGBX"}>
+    <LiveblocksProvider 
+    throttle={16}
+    authEndpoint="/api/liveblocks-auth"
+    >
       <RoomProvider id={params.documentId as string}>
         <ClientSideSuspense fallback={<div>Loading…</div>}>
           {children}
