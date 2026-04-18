cd0435bcb6e0fdeea06d30513f46f299bda057f0
ashwin biju <ashwinbiju011@gmail.com>
2026-04-18 12:57:24 +0530

    for-deployment

--- src/app/documents/[documentId]/avatars.tsx
+++ src/app/documents/[documentId]/avatars.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from "next/image";
 import { Separator } from "@/components/ui/separator";
 import { ClientSideSuspense } from "@liveblocks/react";
 
@@ -56,9 +57,12 @@ const Avatar = ({ src, name }: AvatarProps) => {
             <div className="opacity-0 group-hover:opacity-100 absolute top-full py-1 px-2 text-white text-xs rounded-lg mt-2.5 z-10 bg-black whitespace-nowrap transition-opacity">
                 {name}
             </div>
-            <img 
+            <Image 
             alt={name}
             src={src}
+            width={AVATAR_SIZE}
+            height={AVATAR_SIZE}
+            unoptimized
             className="size-full rounded-full"
             />
         </div>
